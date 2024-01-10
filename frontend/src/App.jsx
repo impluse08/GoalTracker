5f70f610228ca43de1bef0871f0750542217fc20
Impluse08 <pranavsarwe007@gmail.com>
2024-01-10 11:22:30 +0530

    Backend basic configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,73 +1,52 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() 
-{
-  const coords = { x: 0, y: 0 };
-  const circles = document.querySelectorAll(".circle");
-  
-  // const colors = ["#000000","#080808","#191919","#2d2d2d","#474747","#666666","#888888","#ababab","#cbcbcb","#e6e6e6","#f8f8f8","#ffffff",
-  // ];
-  const colors = ["#000000"];
-  
-  circles.forEach(function (circle, index) {
-    circle.x = 0;
-    circle.y = 0;
-    circle.style.backgroundColor = colors[index % colors.length];
-  });
-  
-  window.addEventListener("mousemove", function(e){
-    coords.x = e.clientX;
-    coords.y = e.clientY;
-  });
-  
-  function animateCircles() {
-    
-    let x = coords.x;
-    let y = coords.y;
-    
-    circles.forEach(function (circle, index) {
-      circle.style.left = x - 12 + "px";
-      circle.style.top = y - 12 + "px";
-      
-      circle.style.scale = (circles.length - index) / circles.length;
-      
-      circle.x = x;
-      circle.y = y;
-  
-      const nextCircle = circles[index + 1] || circles[0];
-      x += (nextCircle.x - x) * 0.3;
-      y += (nextCircle.y - y) * 0.3;
-    });
-   
-    requestAnimationFrame(animateCircles);
-  }
-  
-  animateCircles();
-  
+function App() {
 
+  // const coords = { x: 0, y: 0 };
+  // const circles = document.querySelectorAll(".circle");
+  
+  // // const colors = ["#000000","#080808","#191919","#2d2d2d","#474747","#666666","#888888","#ababab","#cbcbcb","#e6e6e6","#f8f8f8","#ffffff",
+  // // ];
+  // const colors = ["#000000"];
+  
+  // circles.forEach(function (circle, index) {
+  //   circle.x = 0;
+  //   circle.y = 0;
+  //   circle.style.backgroundColor = colors[index % colors.length];
+  // });
+  
+  // window.addEventListener("mousemove", function(e){
+  //   coords.x = e.clientX;
+  //   coords.y = e.clientY;
+  // });
+  
+  // function animateCircles() {
+    
+  //   let x = coords.x;
+  //   let y = coords.y;
+    
+  //   circles.forEach(function (circle, index) {
+  //     circle.style.left = x - 12 + "px";
+  //     circle.style.top = y - 12 + "px";
+      
+  //     circle.style.scale = (circles.length - index) / circles.length;
+      
+  //     circle.x = x;
+  //     circle.y = y;
+  
+  //     const nextCircle = circles[index + 1] || circles[0];
+  //     x += (nextCircle.x - x) * 0.3;
+  //     y += (nextCircle.y - y) * 0.3;
+  //   });
+   
+  //   requestAnimationFrame(animateCircles);
+  // }
+  
+  // animateCircles();
+  
   return (
     <>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
-      <div className="circle"></div>
       <div className='flex items-center justify-center h-screen'>
         <div className= '@apply w-[700px] h-[700px] bg-[#48484893] mb-24 rounded-lg'>
           <div>
@@ -81,7 +60,7 @@ function App()
                 </button>
                 <button type="button" className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-20 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 me-2 mb-2">
                 <svg className="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path fillRule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd"/>
+                <path fillrule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clipRule="evenodd"/>
                 </svg>
                 Sign in with Github
                 </button>
