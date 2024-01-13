35640f15b62e9655f92870cbad15482b9495be33
Impluse08 <pranavsarwe007@gmail.com>
2024-01-13 12:25:50 +0530

    registerUser completed

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,2 +1,12 @@
-const userRouter = ()=>{console.log("HIIII");}
-export default userRouter
+import { Router } from "express";
+import {
+    registerUser
+} from "../controllers/user.controller.js"
+import { upload } from "../middlewares/multer.middlerware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router()
+
+router.route("/register").post(registerUser)
+
+export default router 
